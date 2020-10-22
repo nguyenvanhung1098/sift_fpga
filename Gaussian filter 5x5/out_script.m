d73672bae90b04a17c263f34774e368e024de74d
nguyenvanhung1098 <32577895+nguyenvanhung1098@users.noreply.github.com>
2020-10-22 10:52:31 +0700

    add new modle

--- Gaussian filter 5x5/out_script.m
+++ Gaussian filter 5x5/out_script.m
@@ -12,3 +12,21 @@ pixel_out = pixel_out(find(valid_out==1));
 img_out = round(reshape(pixel_out, 640, 480)');
 % hien thi anh dau ra
 figure, imshow(uint8(img_out));
+
+
+
+%--------------------------------------------------------------%
+h1 = fspecial('gaussian', [5 5], 1.1);
+r1 = filter2(h1, I, 'same');
+h2 = fspecial('gaussian', [5 5], 1.3);
+r2 = filter2(h2, I, 'same');
+dog1= r2-r1;
+h3 = fspecial('gaussian', [5 5], 1.1);
+r3 = filter2(h3, I, 'same');
+dog2= r3-r2;
+h4 = fspecial('gaussian', [5 5], 1.3);
+r4 = filter2(h4, I, 'same');
+dog3= r4-r3;
+figure, imshow(uint8(dog1));
+figure, imshow(uint8(dog2));
+figure, imshow(uint8(dog3));
